d2cf93705b21896e3247e31818f5c4ee3675037b
jonsawyer <jonsawyer>
2006-03-30 19:00:00 +0000

    *** empty log message ***

--- ruby4fpc/ruby/tests/parser-test.rb
+++ ruby4fpc/ruby/tests/parser-test.rb
@@ -1,11 +1,75 @@
 #!/usr/local/bin/ruby
 
-require '../convert.rb';
+MAXLENTH = 255;
 
-aLines = IO.readlines("test.h");
+class Token
+	public
+		attr_reader :str, :symbol;
+	private	
+		def initialize(aStr, aSymbol)
+			@str = aStr;
+			@symbol = aSymbol;
+		end;
+end;
 
-aLines.each() { |aLine|
+class Tokenizer < File
+	private
+		def initialize(aFile)
+			super(aFile)
+			@tokenList = Array.new;
+		end;
 
-}
+		def match(aString)
+			token = nil;
+			@tokenList.each() {|aToken|				
+				if aToken.str == aString
+					token = aToken;
+
+				end
+			}
+			return token;
+		end;
+		
+				
+	public
+		
+		def tokenize()
+			theString = String.new;
+			each_byte() { |aByte|
+				if aByte == ?\s then
+					aToken = match(theString)
+					if aToken == nil then
+						Kernel::puts "IDENTIFYER("+theString+")";
+					end;
+					theString = "";					
+				else
+					theString.concat(aByte);
+
+					aToken = match(theString)
+					if aToken != nil
+						Kernel::puts aToken.symbol;
+						theString = "";
+					end
+				end;
+			}
+		end;
+	
+		def addToken(aStr, aSymbol)
+			aToken = Token.new(aStr, aSymbol);
+			@tokenList.push(aToken);					
+		end;
+end;
 
 
+myFile = Tokenizer.open("test.h");
+myFile.addToken( "\n" , "NEWLINE" );
+myFile.addToken( " ", "SPACE" );
+myFile.addToken( "(", "LEFT_PARENTHESES");
+myFile.addToken( ")", "RIGHT_PARENTHESES");
+myFile.addToken( "/", "FORWARD_SLASH");
+myFile.addToken( "*", "ASTERIK");
+myFile.addToken( "+", "PLUS");
+myFile.addToken( "=", "EQUALS");
+myFile.addToken( ";", "SEMICOLON");
+
+myFile.tokenize();
