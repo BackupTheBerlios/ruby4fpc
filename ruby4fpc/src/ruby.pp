0071f2ed7d95ec9b270cea43c8aea59ec9838a20
jonsawyer <jonsawyer>
2006-03-30 23:28:35 +0000

    Modified Files: 	Makefile.fpc src/Makefile.fpc src/intern_h.pp src/ruby.pp  	tests/Makefile.fpc -- Implemented Make File System

--- ruby4fpc/src/ruby.pp
+++ ruby4fpc/src/ruby.pp
@@ -3,7 +3,6 @@
 *********************************************************************)
 (*Original Copyright Notification*************************************
   ruby.h -
-  $Author: jonsawyer $
   created at: Thu Jun 10 14:26:32 JST 1993
   Copyright (C) 1993-2003 Yukihiro Matsumoto
   Copyright (C) 2000  Network Applied Communication Laboratory, Inc.
@@ -25,16 +24,16 @@ Interface
     {$DEFINE RubyExt:=External 'ruby-static'}
     {$DEFINE RubyDcl:=CDECL; RubyExt}
 
-  {$Def ReadImp}
+  {$IfDef ReadImp}
     {$I ruby_h.pp}
     {$I intern_h.pp}
   {$EndIf}
 
 Implementation
 
-  {$Def ReadImp}
+  {$IfDef ReadImp}
     {$I ruby_h.pp}
     {$I intern_h.pp}
   {$EndIf}
-
-End.
+  
+end.
