4a18f83fb42ea4bb213e3b025d1964fdb8f4d375
jonsawyer <jonsawyer>
2006-03-31 02:48:57 +0000

    This is a usefull class.

---
+++ ruby4fpc/process.rb
@@ -0,0 +1,46 @@
+#####################################
+# process.rb
+#####################################
+# Defines how to handle a multi-step
+# process.
+#####################################
+
+class StepByStep
+  private
+    def initialize
+      @steps = Array.new();
+    end
+
+    def finalize
+      puts "ALL DONE :D";
+    end
+
+  public
+    def addStep(cmd, lbl, doFail = true, doRedirect = true)
+      newStep = {"cmd" => cmd, "lbl" => lbl, "doFail" => doFail, "doRedirect" => doRedirect}
+      @steps.push(newStep);
+    end
+
+    def process()
+      @steps.each { |step|
+        puts step["lbl"];
+        cmd = step["cmd"];
+        logName = "/tmp/" + rand().to_s + ".log";
+        cmd += ">& "+logName if step["doRedirect"];
+        system(cmd); ## Execute the Step's Command
+        if step["doFail"] then
+          if $? != 0 then
+            if step["doRedirect"]
+              puts "###### ERROR.LOG START #####";
+              system("cat "+logName);
+              puts "###### ERROR.LOG STOP ######";
+            else
+              puts "###### ERROR HAPPENED ######";
+            end;
+            exit 1;
+          end;
+        end;
+        File.delete(logName) if File.exists?(logName);
+      }
+    end;
+end;
