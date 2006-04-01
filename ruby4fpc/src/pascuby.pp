67957c6cb752a06b7b8b70ec5a893cf9b3f524a6
jonsawyer <jonsawyer>
2006-04-01 20:37:54 +0000

    Begain Implementing Ruby to Pascal Translation Objects.

--- ruby4fpc/src/pascuby.pp
+++ ruby4fpc/src/pascuby.pp
@@ -1,3 +1,5 @@
+{$Mode OBJFPC}
+{$LONGSTRINGS ON}
 (*********************************************************************
   Ruby to Object Pascal Naturalization Project
   Author: Jon (BadQuanta) Sawyer
@@ -7,6 +9,29 @@ Interface
   Uses
     Ruby;
 
+  Type
+    cRubyToPascalObj = Class
+    Protected
+      fValue : VALUE;
+    Public
+      Constructor WrapExisting(aValue: VALUE);
+      Function  Dispatch (aMessage : String; aArg : Array of VALUE): VALUE;
+    end;
+
+
+
 Implementation
+
+  // cRubyToPascalObj -- is exactly what it says.. a wrapper for a
+  // ruby object in pascal.
+  Constructor cRubytoPascalObj.WrapExisting(aValue: VALUE);
+  Begin
+    fValue := aValue;
+  end;
+
+  Function  cRubytoPascalObj.Dispatch (aMessage : String; aArg : Array of VALUE): VALUE;
+  Begin
+
+  end;
 
 end.
